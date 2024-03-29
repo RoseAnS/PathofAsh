55dff13b006b09cf94c928d364bf9aa924d3a212
TallyTS <124575498+RoseAnS@users.noreply.github.com>
2024-03-29 22:14:06 +0000

    Handle tags added, test script updated, and error fixed in clickable

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -18,8 +18,14 @@ public class DialogueController : MonoBehaviour
     [SerializeField] private GameObject flowerHolder;
     [SerializeField] private GameObject spikeHolder;
 
+
+    [Header("Convo Info")]
+    [SerializeField] private string dialogueOutputLocation;
+
     private Story currentConvo;
     private bool dialogueIsPlaying;
+
+    private const string SPEAKER_TAG = "speaker";
 
 
     private void Awake()
@@ -49,6 +55,7 @@ public class DialogueController : MonoBehaviour
         currentConvo = new Story(inkJSON.text); //sets the current story
         dialogueIsPlaying = true; //makes sure we know dialogue is playing
 
+        currentConvo.Continue(); //skips the first line making sure that the tags there are ready to be read
         ContinueStory();
 
     }
@@ -62,7 +69,48 @@ public class DialogueController : MonoBehaviour
     {
         if (currentConvo.canContinue) //add checks for tags here before displaying the next text
         {
-            boxText.text = currentConvo.Continue();
+            HandleTags(currentConvo.currentTags);
+
+            if (dialogueOutputLocation == "Box")
+            {
+                    boxText.text = currentConvo.Continue();
+            }
+            else if (dialogueOutputLocation == "Spike")
+            {
+                    spikeText.text = currentConvo.Continue();
+            }
+            else if (dialogueOutputLocation == "Flower")
+            {
+                    flowerText.text = currentConvo.Continue();
+            }
+            else
+            {
+                    Debug.LogWarning("WARNING. The previous line was untagged and therefore a speaker cannot be assigned.");
+            }
+        }
+    }
+    private void HandleTags(List<string> currentTags)
+    {
+        foreach (string tag in currentTags)
+        {
+            //parse the tag
+            string[] splitTag = tag.Split(':');
+            if (splitTag.Length != 2)
+            {
+                Debug.LogWarning("WARNING. Tag could not be parsed");
+            }
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+            switch (tagKey)
+            {
+                case SPEAKER_TAG:
+                    Debug.Log("speaker=" + tagValue);
+                    dialogueOutputLocation = tagValue;
+                    break;
+                default:
+                    Debug.LogWarning("Tag came in but cannot or is currently not being handled: " + tag);
+                    break;
+            }
         }
     }
 }
